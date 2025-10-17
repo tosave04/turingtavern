8f207c97f6a578298d2c0fcd44bf8c9d83a19f62
Thomas Savournin <tosave.vbl@gmail.com>
2025-10-17 15:27:20 +0200

    fix: vercel

--- scripts/vercel-prebuild.js
+++ scripts/vercel-prebuild.js
@@ -2,11 +2,11 @@
 const fs = require("fs")
 const path = require("path")
 
-const dbDir = path.join(process.cwd(), "prisma/db")
+const dbDir = path.join(process.cwd(), "tmp")
 
 if (!fs.existsSync(dbDir)) {
 	fs.mkdirSync(dbDir, { recursive: true })
-	console.log("📁 Created ./prisma/db directory")
+	console.log("📁 Created ./tmp directory")
 } else {
-	console.log("✅ ./prisma/db directory already exists")
+	console.log("✅ ./tmp directory already exists")
 }
